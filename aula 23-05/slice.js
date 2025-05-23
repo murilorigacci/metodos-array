e629c4d2b7c829060dbb5c87eae52b6465863e1f
Murilo Rigacci <murilo.r.rocha@aluno.senai.br>
2025-05-23 10:24:29 -0300

    Altera um arquivo slice

--- aula 23-05/slice.js
+++ aula 23-05/slice.js
@@ -3,3 +3,4 @@ let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz', 'Raggae', 'Classical',
 console.log(`Parte da playlist (índices 2 a 5): ${playlist.slice(2, 6)}`);
 console.log(`Parte da playlist (índice 0 3): ${playlist.slice(0, 4)} `);
 
+console.log(`Parte da playlist (índice 3): ${playlist.slice(3, 4)}`);
