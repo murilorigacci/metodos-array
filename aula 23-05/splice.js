3d105fd783fc4ba3db2eb4ea8e3e8176f4a5bd26
Murilo Rigacci <murilo.r.rocha@aluno.senai.br>
2025-05-23 10:41:33 -0300

    Altera um arquivo splice

--- aula 23-05/splice.js
+++ aula 23-05/splice.js
@@ -1,14 +1,13 @@
-let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz']
+let playlist = ["Funk", "Hip-Hop", "Pop", "Rock", "Jazz", "Hino"];
 
 console.log(`Antes do splice: ${playlist}`);
 
 // Removendo 2 elemntos a partir do índice 2
 let elementosRemovidos = playlist.splice(2, 2);
-let desafio = playlist.splice(2, 3)
 
 console.log(`Depois do splice: ${playlist}`);
 console.log(`Elementos removidos: ${elementosRemovidos}`);
 
+let desafio = playlist.splice(2, 3);
+console.log(`Antes do splice: ${desafio}`);
 console.log(`Depois do splice do desafio: ${playlist}`);
-
-
