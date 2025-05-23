b6ea841b3717d7fe2e1154213937db64b8e4c9c8
Murilo Rigacci <murilo.r.rocha@aluno.senai.br>
2025-05-23 09:57:06 -0300

    Adiciona um novo método

---
+++ aula 23-05/slice.js
@@ -0,0 +1,5 @@
+let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz', 'Raggae', 'Classical', 'Indie'];
+
+console.log(`Parte da playlist (índices 2 a 5): ${playlist.slice(2, 6)}`);
+console.log(`Parte da playlist (índice 0 3): ${playlist.slice(0, 4)} `);
+
