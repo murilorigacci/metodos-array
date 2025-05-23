e1c9648de9e60c26f71f223a0dc1de99252f7dac
Murilo Rigacci <murilo.r.rocha@aluno.senai.br>
2025-05-23 10:51:54 -0300

    Adiciona um novo método

---
+++ aula 23-05/concat.js
@@ -0,0 +1,6 @@
+let playlist1 = ['Funk', 'Hip-Hop', 'Pop'];
+let playlist2 = ['Rock', 'Jazz'];
+
+console.log(`Playlist combinada: ${playlist1.concat(playlist2)}`);
+console.log(`Playlist combinada: ${playlist1.concat(playlist2).join(', ')}`);
+
